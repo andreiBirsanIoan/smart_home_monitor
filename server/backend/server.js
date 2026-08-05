9ac6adec5154e04194cb103f8eb568bd52f6c7dc
Bîrsan Ioan-Andrei <ioanandrei.birsan@ulbsibiu.ro>
2026-08-06 00:43:39 +0300

    Cards display+progress bar for temperature

--- server/backend/server.js
+++ server/backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mysql from 'mysql2/promise';
 import pool from './database.js';
 import cors from 'cors';
+import 'dotenv/config';
 const app = express();
 app.use(cors());
 app.use(express.json());
--- server/frontend/dateCasa.js
+++ server/frontend/dateCasa.js
@@ -3,7 +3,10 @@ let tempArray=[];
 let timeArray=[];
 let btnData=document.getElementById("btn-data");
 const graficDate=document.getElementById("grafic");
-
+let valTemp=document.getElementById("valoare");
+const progressBar=document.getElementById("progress-bar");
+progressBar.min=10;
+progressBar.max=40;
 async function luareDatele(){
     const response=await fetch("http://localhost:8000/api/senzori");
     const data=await response.json();
@@ -14,32 +17,10 @@ async function luareDatele(){
     document.getElementById('timestamp'.textContent)=date;
 }
 let afisare=document.getElementById("lista-date");
-// async function afisareDate(){
-// const response=await fetch("http://localhost:8000/api/istoric")
-// const data=await response.json(); 
-// objArray=data;
-// tempArray=objArray.map(obj=>obj.temperatura)
-// timeArray=objArray.map(obj=>obj.timestamp)
-//     let html=""
-//     for(let obj of objArray){
-//         html+=`<div class="sensor-data">
-//                 <p>Temperatura: ${obj.temperatura}°C</p>
-//                <p>Miscare:${obj.miscare}</p>
-//                <p>Ora:${obj.timestamp}</p>
-//                </div>`
-        
-//     }
-//     afisare.innerHTML=html;
-    
-// }
-//afisareDate();
-
-//btnData.addEventListener('click',afisareDate);
 async function afisareDate(){
 const response=await fetch("http://localhost:8000/api/istoric")
 const data=await response.json(); 
 objArray=data;
-//date.push(new Date(data.timestamp));
 tempArray=objArray.map(obj=>obj.temperatura)
 timeArray=objArray.map(obj=>{
     const d=new Date(obj.timestamp);
@@ -47,7 +28,10 @@ timeArray=objArray.map(obj=>{
     const minute=d.getMinutes().toString().padStart(2,'0'); 
     return ore+":"+minute;   
 });
+let tempActual=objArray.slice(-1)[0];
+valTemp.textContent+=tempActual.temperatura +"°C";
 
+progressBar.value=tempActual.temperatura;
     new Chart(graficDate,{
     type:'line',
     data:{
@@ -60,7 +44,26 @@ timeArray=objArray.map(obj=>{
     },
     options: {
         responsive: true,
-        maintainAspectRatio: false
+        maintainAspectRatio: false,plugins: {
+            title: {
+                display: true,             /* Forțează afișarea titlului */
+                text: 'ISTORIC EVOLUȚIE TEMPERATURĂ', /* Textul dorit */
+                color: '#ffffff',          /* Culoarea textului (alb curat) */
+                align: 'start',            /* Aliniere la stânga (specific dashboard-urilor) */
+                padding: {
+                    top: 10,
+                    bottom: 30             /* Spațiu între titlu și liniile graficului */
+                },
+                font: {
+                    family: "'Space Grotesque', 'Rajdhani'", /* Fontul tehnic din Figma */
+                    size: 14,
+                    weight: '600'          /* Text ușor îngroșat */
+                }
+            },
+            legend: {
+                display: false             /* Ascunde legenda implicită dacă nu o vrei */
+            }
+        }
     }
 });
 
